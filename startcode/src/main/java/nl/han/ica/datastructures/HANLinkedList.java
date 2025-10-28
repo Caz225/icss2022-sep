93770ce49de790a85fd1ac97e97abd6f938a0a34
Cas Meulenbelt <c.meulenbelt@student.han.nl>
2025-10-29 00:46:44 +0100

    HanLinkedList verbeterd

--- startcode/src/main/java/nl/han/ica/datastructures/HANLinkedList.java
+++ startcode/src/main/java/nl/han/ica/datastructures/HANLinkedList.java
@@ -3,7 +3,6 @@ package nl.han.ica.datastructures;
 import java.util.Iterator;
 import java.util.LinkedList;
 
-// Eenvoudige implementatie van IHANLinkedList
 public class HANLinkedList<T> implements IHANLinkedList<T>, Iterable<T> {
 
     private LinkedList<T> list = new LinkedList<>();
@@ -11,26 +10,6 @@ public class HANLinkedList<T> implements IHANLinkedList<T>, Iterable<T> {
     @Override
     public void addFirst(T value) {
         list.addFirst(value);
-    }
-
-    @Override
-    public void clear() {
-
-    }
-
-    @Override
-    public void insert(int index, T value) {
-
-    }
-
-    @Override
-    public void delete(int pos) {
-
-    }
-
-    @Override
-    public T get(int pos) {
-        return null;
     }
 
     @Override
@@ -44,17 +23,32 @@ public class HANLinkedList<T> implements IHANLinkedList<T>, Iterable<T> {
     }
 
     @Override
+    public T get(int pos) {
+        return list.get(pos);
+    }
+
+    @Override
     public int getSize() {
-        return 0;
+        return list.size();
+    }
+
+    @Override
+    public void clear() {
+        list.clear();
+    }
+
+    @Override
+    public void insert(int index, T value) {
+        list.add(index, value);
+    }
+
+    @Override
+    public void delete(int pos) {
+        list.remove(pos);
     }
 
     @Override
     public Iterator<T> iterator() {
         return list.iterator();
-    }
-
-    @Override
-    public String toString() {
-        return list.toString();
     }
 }
